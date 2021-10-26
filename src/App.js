07081fbcc4f8a3e7b92c791bd58799d25d78e981
JacobBerkey <jacobberkey@yahoo.com>
2021-10-26 14:58:02 -0400

    Sign Up landing page created

--- src/App.js
+++ src/App.js
@@ -1,11 +1,12 @@
 import SignInSide from "./Components/Login/Login";
+import SignUp from "./Components/Login/SignUp";
 import React, { Component } from 'react'
 
 class App extends Component {
   render () {
     return (
       <div className='App'>
-        <SignInSide />
+        <SignUp />
       </div>
     )
   }
