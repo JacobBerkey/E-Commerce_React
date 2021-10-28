8e5a3296ce8ca76821ed21b83c26ceec7af986ef
Atucker91 <alextuck1991@gmail.com>
2021-10-28 09:52:50 -0500

    pulling updates

--- src/App.js
+++ src/App.js
@@ -1,20 +1,35 @@
-import SignInSide from "./components/Login/Login";
+import Login from "./components/Login/Login";
 import SignUp from "./components/Login/SignUp";
 import SearchBar from "./components/SearchBar/SearchBar";
 import NavBar from "./components/NavBar/NavBar";
 import ShoppingCart from "./components/ShoppingCart/ShoppingCart";
 import CreateListing from "./components/CreateListing/CreateListing";
 import Home from "./components/Home/Home";
-import React, { Component } from 'react'
-import {Switch, Route} from 'react-router-dom'
+import React, { Component } from 'react';
+import {Switch, Route} from 'react-router-dom';
 import axios from "axios";
+import jwt_decode from "jwt-decode";
+import {Redirect} from "react-router";
 
 class App extends Component {
   constructor(props){
     super(props);
       this.state = {
-        logged_in_user: [],
+        
       }
+  }
+
+  componentDidMount(){
+    const jwt = localStorage.getItem('token');
+    try{
+      const user = jwt_decode(jwt);
+      this.setState({
+        user
+      });
+      console.log("CompDidMount", user)
+    } catch(err) {
+      console.log("CompDidMount token not found")
+    }
   }
 
   // axios POST create user and GET token
@@ -30,24 +45,23 @@ class App extends Component {
 
 
 
-  render () {
-    const user = this.state.user;
-    return (
-      <div><NavBar /> 
-        <SearchBar />
-      <div className='App'>
-        <Switch>
-          <Route path="/" exact component={Home} />
-          <Route path="/Login" component={SignInSide} />
-          {/* <Route path="/Register" component={SignUp} /> */}
-          <Route path="/Register" render={props => <SignUp {...props} createNewUser={this.createNewUser} />} />
-          <Route path="/shoppingcart" component={ShoppingCart} />
-          <Route path="/create" component={CreateListing} />
-        </Switch>
-      </div>
-      </div>
-    )
-  }
+ render () {
+  const user = this.state.user;
+  return (
+    <div><NavBar /> 
+      <SearchBar />
+    <div className='App'>
+      <Switch>
+        <Route path="/" exact component={Home} />
+        <Route path="/Login" component={Login} />
+        <Route path="/Register" render={props => <SignUp {...props} createNewUser={this.createNewUser} />} />
+        <Route path="/shoppingcart" component={ShoppingCart} />
+        <Route path="/create" component={CreateListing} />
+      </Switch>
+    </div>
+    </div>
+  )
+}
 }
 
 export default App
