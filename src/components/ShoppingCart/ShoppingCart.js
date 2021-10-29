c5d3c6716ecbec811ac8213a54bfea970db4ddee
justincamp1776 <justincamp1776@gmail.com>
2021-10-29 14:53:32 -0400

    Added Shopping Cart GET request + shopping cart table

--- src/components/ShoppingCart/ShoppingCart.js
+++ src/components/ShoppingCart/ShoppingCart.js
@@ -1,7 +1,69 @@
-import React from 'react'
-function ShoppingCart() {
-    return (
-        <h1>Customer Shopping Cart</h1>
-    )
+import React, {Component} from 'react'
+import axios from 'axios';
+import {Table} from 'react-bootstrap';
+
+
+
+class ShoppingCart extends Component {
+    constructor(props) {
+        super(props);
+        this.state = { 
+            shoppingCart : [],
+        }
+    }
+
+
+
+    componentDidMount = () =>{
+        this.getShoppingCart();
+        
+    }
+
+    getShoppingCart = async()=>{
+        const jwt = localStorage.getItem('token');
+        let response = await axios.get(`https://localhost:44394/api/shoppingcart`, {headers: {Authorization: 'Bearer ' + jwt}});
+        this.setState({
+          shoppingCart : response.data
+        })
+        console.log("ShoppingCart :", this.state.shoppingCart)
+       }
+  
+
+   
+
+    render() { 
+        return ( 
+            <div style={{marginRight: "350px", marginLeft: "250px"}} >
+                <Table  striped bordered hover>
+                            <thead>
+                                <tr>
+                                <th>PRODUCT NAME</th>
+                                <th>CATEGORY</th>
+                                <th>DESCRIPTION</th>
+                                <th>QUANTITY</th>
+                                <th>PRICE</th>
+                                </tr>
+                            </thead>
+                            {this.state.shoppingCart.map((product)=>(
+                            <tbody>
+                                <tr>
+                                <td>{console.log("productName :", product.name)}{product.name}</td>
+                                <td>{product.category}</td>
+                                <td>{product.description}</td>
+                                <td>{product.quantity}</td>
+                                <td>{product.extendedPrice}</td>
+                                </tr>   
+                            </tbody>
+                            ))}
+                </Table>
+            </div>
+         );
+    }
 }
+ 
 export default ShoppingCart;
+
+
+
+
+
