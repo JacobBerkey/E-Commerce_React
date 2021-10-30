4c05f50010ed3379856530128aa7f6050aabde86
justincamp1776 <justincamp1776@gmail.com>
2021-10-30 13:39:34 -0400

    Moved the search bar under the title

--- src/components/ShoppingCart/ShoppingCart.js
+++ src/components/ShoppingCart/ShoppingCart.js
@@ -40,8 +40,8 @@ class ShoppingCart extends Component {
 
     render() { 
         return ( 
-            <div style={{marginRight: "450px", marginLeft: "250px"}} >
-                <h1 style={{marginLeft: "100px", marginBottom: "100px", marginTop: "80px"}}>Shopping Cart</h1>
+            <div style={{marginRight: "450px", marginLeft: "250px", marginBottom: "250px"}} >
+                <h1 style={{marginLeft: "100px", marginBottom: "100px", marginTop: "80px", fontFamily: "inherit"}} >Shopping Cart</h1>
                 <Table  striped bordered hover>
                             <thead>
                                 <tr>
